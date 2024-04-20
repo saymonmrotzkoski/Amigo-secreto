96be7e304d9d71bb7adf18b2b04509d1e0aabc27
Saymon Mrotzkoski <149203236+saymonmrotzkoski@users.noreply.github.com>
2024-04-20 11:44:35 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+ numero = prompt('digite um numero');
+
+function valorDoNumero(){
+    if (numero < 0){
+        alert('O numero é positivo!');
+    } else if (numero > 0) {
+        alert('O numero é negativo!')
+
+    }
+}
